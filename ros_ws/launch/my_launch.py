15821e6a8e0a32747a6ea778bbd08a59c317c06d
luckykoritela <70424536+luckykoritela@users.noreply.github.com>
2022-12-17 21:37:21 -0500

    Create my_launch.py

---
+++ ros_ws/launch/my_launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+	return LaunchDescription([
+		Node(
+			package='cv_basics',
+			executable='image_subscriber',
+			name='sub',
+			output='screen'
+		),
+		Node(
+			package='cv_basics',
+			namespace='publisher',
+			executable='image_publisher',
+			name='pub1',
+			output='screen'
+		),
+		Node(
+			package='cv_basics',
+			namespace='publisher',
+			executable='image_publisher',
+			name='pub2',	
+			output='screen'
+		),
+		Node(
+			package='cv_basics',
+			namespace='publisher',
+			executable='image_publisher',
+			name='pub3',
+			output='screen'
+		)
+	])
