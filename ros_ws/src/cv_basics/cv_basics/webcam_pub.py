9b60b4589c031e79df71bcbff3f484f2b2fef384
luckykoritela <70424536+luckykoritela@users.noreply.github.com>
2022-12-17 21:43:54 -0500

    Update webcam_pub.py

--- ros_ws/src/cv_basics/cv_basics/webcam_pub.py
+++ ros_ws/src/cv_basics/cv_basics/webcam_pub.py
@@ -8,70 +8,72 @@ import psutil
 import os
 import tracemalloc
  
-class ImgSubscriber(Node):
+class ImgPublisher(Node):
   """
-  Create an ImageSubscriber class, which is a subclass of the Node class.
+  Create an ImagePublisher class, which is a subclass of the Node class.
   """
   def __init__(self):
     """
     Class constructor to set up the node
     """
-    super().__init__('image_subscriber')
+    super().__init__('image_publisher')
       
-    self.subscription = self.create_subscription(
-      Image, 
-      'video_frames', 
-      self.listener_callback, 
-      10)
-    self.subscription
+    self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
       
+    timer_period = 0.01
+      
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+         
+    self.cap = cv2.VideoCapture(0)
+         
     self.br = CvBridge()
-    
    
-  def listener_callback(self, data):
-    start = time.perf_counter()
-    tracemalloc.start()
-    
-    face_cascade = cv2.CascadeClassifier('/home/luckykoritela/ros_ws/src/cv_basics/cv_basics/haarcascade_frontalface_default.xml')
+  def timer_callback(self):
     """
     Callback function.
+    This function gets called every 0.1 seconds.
     """
-    self.get_logger().info('Receiving video frame')
+    ret, frame = self.cap.read()
+          
+    if ret == True:
+      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
  
-    current_frame = self.br.imgmsg_to_cv2(data)
-    
-    image_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(image_gray)
-    print(f"{len(faces)} faces detected in the image.")
-    
-    # Display image
-    #for (x,y,w,h) in faces:
-    #	cv2.rectangle(current_frame, (x,y), (x+w,y+h), (0,255,0), 2)
-    #cv2.imshow("camera", current_frame)
-    
-    cv2.waitKey(1)
-    
-    print('RAM memory % used: ', psutil.virtual_memory()[2])
-    print('RAM Used (GB): ', psutil.virtual_memory()[3]/1000000000)
-    print(tracemalloc.get_traced_memory())
-    tracemalloc.stop()
-  
-    end = time.perf_counter()
-    print("Operation Time: " + str(end-start))
+    # Display the message on the console
+    self.get_logger().info('Publishing video frame')
   
 def main(args=None):
+
+  #tracemalloc.start()
   
   # Initialize the rclpy library
   rclpy.init(args=args)
   
   # Create the node
-  image_subscriber = ImgSubscriber()
+  image_publisher = ImgPublisher()
   
   # Spin the node so the callback function is called.
-  rclpy.spin(image_subscriber)
+  print('pid = ', os.getpid())
+  start = time.perf_counter()
+  for i in range(500):
+  	tracemalloc.start()
+  	rclpy.spin_once(image_publisher)
+  	print('RAM memory % used: ', psutil.virtual_memory()[2])
+  	print('RAM Used (GB): ', psutil.virtual_memory()[3]/1000000000)
+  	print(tracemalloc.get_traced_memory())
+  	tracemalloc.stop()
+  end = time.perf_counter()
+  
+  print("Total Time: " + str(end-start))
+  
+  #snapshot = tracemalloc.take_snapshot()
+  #top_stats = snapshot.statistics('lineno')
+  
+  #print("[ Top 10 ]")
+  #for stat in top_stats[:10]:
+  #	print(stat)
   
   # Destroy the node
-  image_subscriber.destroy_node()
+  image_publisher.destroy_node()
   
   # Shutdown the ROS client library for Python
   rclpy.shutdown()
