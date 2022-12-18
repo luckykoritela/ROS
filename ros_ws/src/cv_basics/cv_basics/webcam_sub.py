974967b684e8ffc873b8012cf2996e9ed4a0508d
luckykoritela <70424536+luckykoritela@users.noreply.github.com>
2022-12-17 21:42:05 -0500

    Update webcam_sub.py

--- ros_ws/src/cv_basics/cv_basics/webcam_sub.py
+++ ros_ws/src/cv_basics/cv_basics/webcam_sub.py
@@ -1,21 +1,14 @@
-# Basic ROS 2 program to subscribe to real-time streaming 
-# video from your built-in webcam
-# Author:
-# - Addison Sears-Collins
-# - https://automaticaddison.com
-  
-# Import the necessary libraries
-import rclpy # Python library for ROS 2
-from rclpy.node import Node # Handles the creation of nodes
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 import time
 import psutil
 import os
 import tracemalloc
  
-class ImageSubscriber(Node):
+class ImgSubscriber(Node):
   """
   Create an ImageSubscriber class, which is a subclass of the Node class.
   """
@@ -23,19 +16,15 @@ class ImageSubscriber(Node):
     """
     Class constructor to set up the node
     """
-    # Initiate the Node class's constructor and give it a name
     super().__init__('image_subscriber')
       
-    # Create the subscriber. This subscriber will receive an Image
-    # from the video_frames topic. The queue size is 10 messages.
     self.subscription = self.create_subscription(
       Image, 
       'video_frames', 
       self.listener_callback, 
       10)
-    self.subscription # prevent unused variable warning
+    self.subscription
       
-    # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
     
    
@@ -47,10 +36,8 @@ class ImageSubscriber(Node):
     """
     Callback function.
     """
-    # Display the message on the console
     self.get_logger().info('Receiving video frame')
  
-    # Convert ROS Image message to OpenCV image
     current_frame = self.br.imgmsg_to_cv2(data)
     
     image_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
@@ -78,14 +65,12 @@ def main(args=None):
   rclpy.init(args=args)
   
   # Create the node
-  image_subscriber = ImageSubscriber()
+  image_subscriber = ImgSubscriber()
   
   # Spin the node so the callback function is called.
   rclpy.spin(image_subscriber)
   
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
+  # Destroy the node
   image_subscriber.destroy_node()
   
   # Shutdown the ROS client library for Python
