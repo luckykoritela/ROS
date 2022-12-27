ff485644d628ae40484684381a5841f463337c4c
luckykoritela <70424536+luckykoritela@users.noreply.github.com>
2022-12-26 23:42:42 -0500

    Update webcam_sub.py

--- ros_ws/src/cv_basics/cv_basics/webcam_sub.py
+++ ros_ws/src/cv_basics/cv_basics/webcam_sub.py
@@ -1,8 +1,9 @@
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-import cv2
+# Import the necessary libraries
+import rclpy # Python library for ROS 2
+from rclpy.node import Node # Handles the creation of nodes
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2 # OpenCV library
 import time
 import psutil
 import os
@@ -16,15 +17,19 @@ class ImgSubscriber(Node):
     """
     Class constructor to set up the node
     """
+    # Initiate the Node class's constructor and give it a name
     super().__init__('image_subscriber')
       
+    # Create the subscriber. This subscriber will receive an Image
+    # from the video_frames topic. The queue size is 10 messages.
     self.subscription = self.create_subscription(
       Image, 
       'video_frames', 
       self.listener_callback, 
       10)
-    self.subscription
+    self.subscription # prevent unused variable warning
       
+    # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
     
    
@@ -36,8 +41,10 @@ class ImgSubscriber(Node):
     """
     Callback function.
     """
+    # Display the message on the console
     self.get_logger().info('Receiving video frame')
  
+    # Convert ROS Image message to OpenCV image
     current_frame = self.br.imgmsg_to_cv2(data)
     
     image_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
@@ -70,7 +77,9 @@ def main(args=None):
   # Spin the node so the callback function is called.
   rclpy.spin(image_subscriber)
   
-  # Destroy the node
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
   image_subscriber.destroy_node()
   
   # Shutdown the ROS client library for Python
